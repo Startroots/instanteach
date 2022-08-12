4a564c671ea87b7a7b7260a41b045a6fc61cd6e0
joigmz <jose.izam99@gmail.com>
2022-08-11 20:39:03 -0400

    tutor leadform

--- wsp.js
+++ wsp.js
@@ -11,3 +11,17 @@ function gotowhatsapp() {
 
     window.open(url, '_blank').focus();
 }
+
+function gotowhatsappTutors() {
+    
+    var name = document.getElementById("tutor_name").value;
+    var university = document.getElementById("tutor_university").value;
+    var educational_level = document.getElementById("tutor_educational_level").value;
+    var course = document.getElementById("tutor_course").value;
+
+    var url = "https://wa.me/+56979945385?text=" 
+    + "Hola mi nombre es " + name +", soy alumno "+ educational_level +" de la "
+    + university +" y estoy interesado en realizar clases como tutor del curso " + course + "." 
+
+    window.open(url, '_blank').focus();
+}
